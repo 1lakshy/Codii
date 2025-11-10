6c8e6efa751df3bf0cc2e08a1451b0f65be0a83e
1lakshy <lakshy18sharma@gmail.com>
2025-11-10 23:48:00 +0530

    Final App.js with New navigation system with side bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import JavaCompiler from './Pages/JavaCompiler'
 import PythonCompiler from './Pages/PythonCompiler'
-import './App.css'
 import JsCompiler from './Pages/JSCompiler'
+import './App.css'
+import Sidebar from './Components/SideBar'
+import NotFound from './Pages/NotFound'
 
 const App = () => {
   return (
-   <>
-   <Routes>
-        <Route path="/" element={<JavaCompiler />} />
-        <Route path="/python" element={<PythonCompiler />} />
-        <Route path="/js" element={<JsCompiler />} />
-        <Route
-          path="*"
-          element={<JavaCompiler />}
-        />
-    </Routes>
-   </>
+    <div className="app-container">
+      <Sidebar />
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<JavaCompiler />} />
+          <Route path="/compile/python" element={<PythonCompiler />} />
+          <Route path="/compile/js" element={<JsCompiler />} />
+          <Route path="/compile/java" element={<JavaCompiler />} />
+          <Route path="/*" element={<NotFound/>} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
